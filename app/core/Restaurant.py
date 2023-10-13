32a4a17d7f6687fe4dd55c9f702b4e6d281bee1a
luckyrp2 <147841151+luckyrp2@users.noreply.github.com>
2023-10-13 18:15:42 -0500

    Update Restaurant.py

--- app/core/Restaurant.py
+++ app/core/Restaurant.py
@@ -5,20 +5,25 @@ from app.api import restaurant_crud
 from app.database import configuration
 
 router = APIRouter(tags=["Restaurant"], prefix="/restaurant")
+
+#creating a database
 get_db = configuration.get_db
 
+#response_model controls input
 @router.post("/new",
              response_model=Restaurant,
              summary="Make a new Restaurant",
              status_code=status.HTTP_201_CREATED)
+#pydantic verificiation first entry
 def r_restaurant(restaurant: Restaurant, db: Session = Depends(get_db)):
-    db_restaurant = restaurant_crud.create_restaurant(db,
+    #check if restaurant exists
+    db_restaurant = restaurant_crud.get_restaurant_by_id(db,
                                              id = restaurant.id,
                                              date = restaurant.date_added,
                                              name = restaurant.name,
                                              chef_name = restaurant.chef_name)
     if db_restaurant:
         raise HTTPException(status_code=status.HTTP_409_CONFLICT,
-                            detail="Email already registered")
-    return restaurant_crud.create_pilot_user(db=db, restaurant=restaurant)
+                            detail="Restaurant already exists")
+    return restaurant_crud.create_restaurant(db=db, restaurant=restaurant)
 
